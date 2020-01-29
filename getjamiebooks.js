2d4b0c145c0c82c8802299458207aa63a2c6e34a
jmacleod96 <60449814+jmacleod96@users.noreply.github.com>
2020-01-29 21:34:49 +0000

    First commit

---
+++ getjamiebooks.js
@@ -0,0 +1,22 @@
+$("document").ready(function() {
+
+ $.ajax({
+	url: 'https://www.goodreads.com/review/list/72827801.xml?key=s0JsLuTDaZxAxCCWKFzA&v=2&shelf=read&per_page=200&page=1',
+	dataType: 'xml',
+	success: function(data) {
+	var txt = "";
+	var booksdata = data;
+	var name1 = booksdata.getElementsByTagName('title').length;
+	document.getElementById("jamiebooksread").innerHTML = "Jamie has read " + name1 + " books so far!" + "<br>";
+	var name = booksdata.getElementsByTagName('title');
+	for (i = 0 ; i <name.length; i++) {
+        txt += name[i].childNodes[0].nodeValue + "<br>";
+    }
+	document.getElementById("jamiebookshere").innerHTML = txt;
+	console.log(booksdata);
+	},
+	error: function(data) {
+	console.log('Error loading XML data');
+	}
+ })
+})
