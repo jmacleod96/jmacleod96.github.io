875e78d2299216f1f0e89658caca85293d7ad8ae
jmacleod96 <60449814+jmacleod96@users.noreply.github.com>
2020-01-30 22:37:18 +0000

    Update getjamiebooks.js

--- getjamiebooks.js
+++ getjamiebooks.js
@@ -10,7 +10,7 @@ $("document").ready(function() {
 	document.getElementById("jamiebooksread").innerHTML = "Jamie has read " + name1 + " books so far!" + "<br>";
 	var name = booksdata.getElementsByTagName('title');
 	
-	var pages1 = booksdata.getElementsByTagName('num_pages').length;
+	var pages1 = booksdata.getElementsByTagName('num_pages');
 	document.getElementById("jamiepagesread").innerHTML = "Pages read so far:" + pages1 + "<br>"; 
 	
 	
