27a473c3903f898e254b72782da0e876215caad4
jmacleod96 <60449814+jmacleod96@users.noreply.github.com>
2020-02-01 17:48:43 +0000

    Update getannabooks.js

--- getannabooks.js
+++ getannabooks.js
@@ -1,7 +1,7 @@
 $("document").ready(function() {
 
  $.ajax({
-	url: 'https://www.goodreads.com/review/list/11465889.xml?key=s0JsLuTDaZxAxCCWKFzA&shelf=read&per_page=infinite',
+	url: 'https://www.goodreads.com/review/list/11465889.xml?key=s0JsLuTDaZxAxCCWKFzA&v=2&read_at=2020',
 	dataType: 'xml',
 	success: function(data) {
 	var txt = "";
