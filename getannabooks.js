4bce51a0186257b336477cdf3b86d3336a89d35a
jmacleod96 <60449814+jmacleod96@users.noreply.github.com>
2020-03-04 20:49:04 +0000

    Update getannabooks.js

--- getannabooks.js
+++ getannabooks.js
@@ -14,7 +14,7 @@ $("document").ready(function() {
 	for (let i = 0; i < numpages.length; i++) { 
             num += numpages[i];
 			console.log(num);
-			console.log(numpages[i]); 
+			
         } 
 	
 	
