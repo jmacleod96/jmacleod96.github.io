f98b08c4d6b353ca5424a47b99fa9ea0f5519d84
jmacleod96 <60449814+jmacleod96@users.noreply.github.com>
2020-03-05 19:21:01 +0000

    l

--- getannabooks.js
+++ getannabooks.js
@@ -10,10 +10,7 @@ $("document").ready(function() {
 	document.getElementById("annabooksread").innerHTML = "Books read: " + name1 + "<br>" ;
 
     var numpages = booksdata.getElementsByTagName('num_pages').innerHTML;
-	for (let i = 0; i < numpages.length; i++) { 
-            console.log(numpages[i]); 
-			document.getElementById("annapagesread").innerHTML = numpages;
-        } 
+	console.log(numpages);
 	
 	
 	
