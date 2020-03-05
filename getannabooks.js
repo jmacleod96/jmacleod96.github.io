355318ff16d4bd8fc46d4be68810c030889bdead
jmacleod96 <60449814+jmacleod96@users.noreply.github.com>
2020-03-05 19:34:06 +0000

    Update getannabooks.js

--- getannabooks.js
+++ getannabooks.js
@@ -10,7 +10,7 @@ $("document").ready(function() {
 	document.getElementById("annabooksread").innerHTML = "Books read: " + name1 + "<br>" ;
 
     var numpages = booksdata.getElementsByTagName('num_pages').innerHTML;
-	console.log(numpages);
+	console.log.toString(numpages);
 	
 	
 	
