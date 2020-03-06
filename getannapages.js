3ec00448f3c44143189722d5c5ead95a070f8ce6
jmacleod96 <60449814+jmacleod96@users.noreply.github.com>
2020-03-06 19:42:00 +0000

    Update getannapages.js

--- getannapages.js
+++ getannapages.js
@@ -10,7 +10,7 @@ $("document").ready(function() {
 	
 	var numpages = booksdata.getElementsByTagName('num_pages')[i].innerHTML;
 	for (i = 0 ; i <numpages.length; i++) {
-        txt += numpages[i].childNodes[0].nodeValue + "<br>";
+        txt += numpages + "<br>";
     }
 	document.getElementById("annapageshere").innerHTML = txt;
 	console.log(numpages);
