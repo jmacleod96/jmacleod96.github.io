eb66a9ddb7278d498c0a7882614c4068c756ccde
jmacleod96 <60449814+jmacleod96@users.noreply.github.com>
2020-03-06 19:27:06 +0000

    v

---
+++ getannapages.js
@@ -0,0 +1,22 @@
+$("document").ready(function() {
+
+ $.ajax({
+	url: 'https://www.goodreads.com/review/list/11465889.xml?key=s0JsLuTDaZxAxCCWKFzA&v=2&read_at=2020',
+	dataType: 'xml',
+	success: function(data) {
+	var txt = "";
+	var booksdata = data;
+		
+	
+	var numpages = booksdata.getElementsByTagName('num_pages');
+	for (i = 0 ; i <numpages.length; i++) {
+        txt += numpages[i].childNodes[0].nodeValue + "<br>";
+    }
+	document.getElementById("annapageshere").innerHTML = txt;
+	console.log(numpages);
+	},
+	error: function(data) {
+	console.log('Error loading XML data');
+	}
+ })
+})
